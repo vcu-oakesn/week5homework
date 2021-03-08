50ed9bf9b1903db7d0cb35cf8894950caf20401f
vcu-oakesn <oakesn@mymail.vcu.edu>
2021-03-07 21:38:42 -0500

    Uno mas

--- fun/homework.py
+++ fun/homework.py
@@ -7,8 +7,8 @@ def find_greatest_number(incoming_list):
     Find the largest number in the list.
     """
     
-    incoming_list = [1, 2, 3]
-    return max(incoming_list)
+    find_greatest_number = max(incoming_list)
+    return find_greatest_number
 
     pass
 
@@ -19,8 +19,8 @@ def find_least_number(incoming_list):
     Find the smallest/least number in the list.
     """
 
-    incoming_list = [4, 5, 6]
-    return min(incoming_list)
+    find_least_number = min(incoming_list)
+    return find_least_number
 
     pass
 
@@ -31,8 +31,8 @@ def add_list_numbers(incoming_list):
     Add all the values together and return it.
     """
 
-    incoming_list = [7, 8, 9, 10]
-    return sum(incoming_list)
+    add_list_numbers = sum(incoming_list)
+    return add_list_numbers
     
     pass
 
@@ -42,13 +42,8 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    # Dictionary includes pop artists with the highest album sales (in millions)
-    incoming_dict = {
-        'Beatles': 283.2, 
-        'Elvis Presley': 227.3, 
-        'Michael Jackson': 236.6, 
-        'Elton John': 194.7, 
-        'Madonna': 178.9}
-    return max(incoming_dict, key=incoming_dict.get)
+
+    longest_value_key = max(incoming_dict, key=incoming_dict.get)
+    return longest_value_key
 
     pass
